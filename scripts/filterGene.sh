bbab666580e17b69aab964dc1dbf8f3f80ec33fc
Emil Gustavsson <68326320+egustavsson@users.noreply.github.com>
2023-04-06 15:47:28 +0100

    Create filterGene.sh
    added new script to filte bu gene

---
+++ scripts/filterGene.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+helpFunction()
+{
+	echo ""
+	echo "Usage: $0 -i input_file -o output_file -g gene_name"
+	echo -e "\t-i input file name"
+	echo -e "\t-o output file name"
+	echo -e "\t-g gene name to filter on"
+	exit 1
+}
+
+while getopts "i:o:g:" opt
+do
+	case "$opt" in
+		i ) input_file="$OPTARG" ;;
+		o ) output_file="$OPTARG" ;;
+		g ) gene_name="$OPTARG" ;;
+		? ) helpFunction ;;
+	esac
+done
+
+if [ -z "$input_file" ] || [ -z "$output_file" ] || [ -z "$gene_name" ]
+then
+   echo "Some or all of the parameters are empty";
+   helpFunction
+fi
+
+zcat "$input_file" | awk -F "\t" -v gene="$gene_name" 'NR == 1 || $2 == gene {print}' > "$output_file"
