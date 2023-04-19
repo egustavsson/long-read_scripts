470b543f5a231a28e29e2d1cc0835b75c0049f91
Emil Gustavsson <68326320+egustavsson@users.noreply.github.com>
2023-04-19 14:02:45 +0100

    Create getClinVarForLoci.sh
    added script to download clinvar data and filter for loci of interest

---
+++ scripts/getClinVarForLoci.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Set default values
+directory="."
+download_url="https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/clinvar.vcf.gz"
+
+# Parse command line options
+while getopts "d:c:s:e:u:" opt; 
+do
+  case $opt in
+    d) directory=$OPTARG ;;
+    c) chr=$OPTARG ;;
+    s) start=$OPTARG ;;
+    e) end=$OPTARG ;;
+    u) download_url=$OPTARG ;;
+    *)
+      echo "Invalid option: -$opt" >&2
+      exit 1
+      ;;
+  esac
+done
+
+# Download file
+wget -O "$directory/clinvar_20230416.vcf.gz" "$download_url"
+
+# Filter vcf file using tabix
+tabix -p vcf "clinvar_20230416.vcf.gz"
+
+tabix "clinvar_20230416.vcf.gz" "$chr:$start-$end" > "$directory/filtered.vcf.gz"
+
+tabix -p vcf "$directory/filtered.vcf.gz"
